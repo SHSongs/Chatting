b1bbe6c91b3028f8984e4edcc3976d61ea61bd51
SONG <simpledeveloper.songs@gmail.com>
2021-09-24 09:24:59 +0900

    check user count

--- 02_socketio.js
+++ 02_socketio.js
@@ -12,19 +12,29 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+var cnt = 0;
+
 io.on('connection', (socket) => {
   console.log('a user connected_');  
+  
+  cnt += 1;
+
+  io.emit('chat message', '-----join new user-----');
+  io.emit('cnt', cnt);
+  
 
   socket.on('disconnect', (socket) => {
     console.log('disconnect')
+    cnt -= 1;
+    io.emit('cnt', cnt);
   })
 
 
   
   socket.on('chat message', (msg) => {
     console.log(msg);
-    
-    
+
+        
     if(msg === preStr || msg.length > 50){
       console.log("폐기")
       return
